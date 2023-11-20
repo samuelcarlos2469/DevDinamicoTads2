0cf6861c90fb174d6f2b4b286be3690b7cc46718
RaiFernandesdosSantos <raifernandes076@gmail.com>
2023-11-20 16:21:16 -0400

    Trabalho finalizado

---
+++ src/js/qrcode.js
@@ -0,0 +1,28 @@
+const qrContainer = document.querySelector("#qr-code");
+const pix = document.querySelector("#pix");
+const qrCode = document.querySelector("#qrPix");
+const finalizar = document.querySelector("#btnFinalizar");
+const parcelas = document.querySelector("#parcelas");
+
+async function generateQRCode() {
+  qrContainer.innerHTML = "";
+  new QRCode("qr-code", {
+    text: "raifernandes076@gmail.com",
+    height: 250,
+    width: 250,
+    colorLight: "#ffffff",
+    colorDark: "#000000",
+  });
+}
+
+pix.addEventListener("change", () => {
+  parcelas.disabled = true;
+  if (pix.checked) {
+    generateQRCode();
+    qrCode.classList.remove("d-none");
+    finalizar.classList.remove("d-none");
+  } else {
+    qrCode.classList.add("d-none");
+    finalizar.classList.add("d-none");
+  }
+});
